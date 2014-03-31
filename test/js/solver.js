b9fb2af5c907c3826f04a1b0df502d5b2f853bcb
Maik Schreiber <blizzy@blizzy.de>
2014-03-31 19:54:03 +0200

    update copyright years

--- test/js/solver.js
+++ test/js/solver.js
@@ -1,6 +1,6 @@
 /*
 Engine cluster calculator for Kerbal Space Program
-Copyright (C) 2012-2013 Maik Schreiber
+Copyright (C) 2013-2014 Maik Schreiber
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
